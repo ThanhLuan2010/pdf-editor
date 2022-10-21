6403716544b3f06c2c36e1361cab74220479c456
ThanhLuan2010 <83200499+ThanhLuan2010@users.noreply.github.com>
2022-10-21 11:12:12 +0700

    Update .DS_Store

---
+++ android/app/src/main/java/com/pitech_jogtoearn_mobile/MainActivity.java
@@ -0,0 +1,9 @@
+package com.pitech_jogtoearn_mobile;
+
+import com.reactnativenavigation.NavigationActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+
+public class MainActivity extends NavigationActivity {
+
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+import {Navigation} from 'react-native-navigation';
+import 'react-native-gesture-handler';
+
+import {setRoot, currentScreen} from './src/utils/navigator';
+import {registeredScreens} from './src/views/registeredScreens';
+
+registeredScreens();
+Navigation.events().registerAppLaunchedListener(() => {
+  setRoot('HomeView');
+});
+Navigation.events().registerComponentDidAppearListener(
+  ({componentId, componentName, passProps}) => {
+    if (componentName && componentId) {
+      currentScreen.set('component', {
+        componentId,
+        componentName,
+        passProps,
+      });
+    }
+  },
+);
